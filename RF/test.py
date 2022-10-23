20b6a059b0bbac6df2eb8abd8782dde1861bec83
loganfarrow <89869123+loganfarrow@users.noreply.github.com>
2022-10-23 12:33:18 -0500

    Update test.py

--- RF/test.py
+++ RF/test.py
@@ -1,7 +1,13 @@
+#run this bash command first
+# rtl_fm -f 144.390M -s 22050 | multimon-ng -t raw -a AFSK1200 -f alpha /dev/stdin > data.txt
+
+#HOW IT WORKS
+#uses rtl_fm software to listen on freq 144.390(megahertz) at a sample rate of 22050 (universal standard) then pipe that information into software
+#multimon-ng and decode it using the AFSK1200 standard then pipe that output to a text file
+
+#PYTHON PORTION
+
 #open text file in read mode
-from hmac import trans_36
-
-
 text_file = open("data.txt", "r")
  
 #read whole file to a string
