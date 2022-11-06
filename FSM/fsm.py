ce249c86f1c9dc95071994e28f542120234ef12a
jiwenofu <c.iwenofu@wustl.edu>
2022-11-06 13:58:14 -0600

    FSM Code #1

--- FSM/fsm.py
+++ FSM/fsm.py
@@ -11,6 +11,8 @@
 import random
 from enum import Enum
 
+signalBuffer = [] #Queue to hold all other signals
+callsign = "NASA22"
 
 # States enum
 class State(Enum):
@@ -24,25 +26,9 @@ class State(Enum):
 def receiveRF():
 	randomInputs = [
 		[],	# empty input
-		["WU22RC", "A1", "G7", "D4", "F6"],
-		''' TODO: add extra test cases here (sequences that are not just our
-					callsign) 
-		'''
+		["NASA22", "A1", "G7", "D4", "F6"],
+		["TRASH", "NASA22", "A1", "F6", "D4"]
 	]
-	
-	#run rtl_recieve.sh first
-
-	#open text file in read mode
-	text_file = open("transmission.txt", "r")
-
-	#read whole file to a string
-	transmission = text_file.read().split()
-	
-	#close file
-	text_file.close()
-
-	# return transmission
-
 	return randomInputs[random.randint(0, len(randomInputs))]
 
 
@@ -51,35 +37,53 @@ def receiveRF():
 # Reads each of the signal's components for that given signal and executes it
 def FSM(state): 
 	nextState	# the next state to be transitioned to
-	
 	# This is essentially a switch statement, but not available < Python 3.10
 	if state == State.WAIT:
-		if (receiveRF()):
+		input = receiveRF()
+		if (input):
 			# if signal received (i.e. not empty list)
+			signalBuffer.append(input)
 			nextState = State.CALL
 		else:
 			# else, keep on waiting
 			nextState = State.WAIT
 
 	elif state == State.CALL:
-		if ( ''' TODO: what should this condition be? '''):
-			nextState = State.READ
-		else:
-			nextState = State.WAIT
+		for pkt in signalBuffer[0]:
+			if (pkt == callsign):
+				nextState = State.EXEC
 
-	elif state == State.READ:
-		if ( ''' TODO: what should this condition be? ''' ):
-			nextState = State.EXEC
-		else:
-			nextState = State.WAIT
+		nextState = State.WAIT
 
 	elif state == State.EXEC:
-
-		''' TODO: fill this out so that you simulate a RAFCO and then 
-					remove it from the list 
-		'''
-
-		nextState = State.READ
+		for RAFCO in signalBuffer[0]:
+			if (RAFCO == "A1"):
+				print("exec1")
+				#execA1()
+			elif (RAFCO == "B2"):
+				print("exec2")
+				#execB2()
+			elif (RAFCO == "C3"):
+				print("exec3")
+				#execC3()
+			elif (RAFCO == "D4"):
+				print("exec4")
+				#execD4()
+			elif (RAFCO == "E5"):
+				print("exec5")
+				#execE5()
+			elif (RAFCO == "F6"):
+				print("exec6")
+				#execF6()
+			elif (RAFCO == "G7"):
+				print("exec7")
+				#execG7()
+			elif (RAFCO == "H8"):
+				print("exec8")
+				#execH8()
+		
+		signalBuffer.remove(0)
+		nextState = State.WAIT
 
 	else:
 		# default cause to wait
@@ -88,12 +92,49 @@ def FSM(state):
 
 def main():
 	currentState = State.WAIT	#Initial waiting condition
-	teamSign = "WU22RC"		# Assume this is our callsign
-	
+
 	# Main loop that continuously runs FSM
 	while True:
 		currentState = FSM(currentState)
+		
 
+'''
+WAIT = 0
+CALL = 1
+READ = 2
+EXEC = 3
 
+def recieveRF(signal): #Lists out the signals and its components to it (called executive)
+	if(signal == "XD71"):
+		executive = ["A1", "G7", "B2"]
+
+	elif (signal == "XX4XXX"):
+		executive = ["C3", "A1", "D4", "C3", "F6", "C3",...]
+
+	return signal
+
+def FSM(state): #Reads each of the signal's components for that given signal and executes it
+	if(state == EXEC):
+		for i in executive:
+			return i
+	
+	return state
+
+def main():
+	currentState = WAIT #Initial waiting condition
+
+	while True:
+		call = "XD71" #Brief outline of the call that we will recieve from our call variable
+		data = recieveRF(call) #Put in that call into this function
+		currentState = CALL #Changes currentState when call recieved
+
+		if(data == teamRF(call)): #Checks to see if the call is ours
+			currentState = EXEC 
+			FSM(currentState) #Will make currentState to execute condition, follows FSM function (see lines 15-20)
+			currentState = WAIT #Back to wait condition once it's done, restarting the cycle
+
+		else:
+			currentState = WAIT #Goes back to wait condition if call is not ours
+'''
 
 	
