eb041339f74296bfcb59ac80e516405de19bb3b8
breadnmkii <b.z.yang@wustl.edu>
2023-01-10 20:31:05 -0600

    RFM receive work

--- dev/Radio/Telemetry/rfm96w.py
+++ dev/Radio/Telemetry/rfm96w.py
@@ -15,6 +15,7 @@
 
 import sys
 import time
+import datetime
 import busio
 import board
 from digitalio import DigitalInOut
@@ -22,26 +23,7 @@ from digitalio import DigitalInOut
 # Import the RFM9x radio module
 import adafruit_rfm9x
 
-def transmit():
-    # Initialize radio
-    spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
-    RST = DigitalInOut(board.D25)
-    CS = DigitalInOut(board.CE1)
-
-    # Configure radio frequencies
-    rf_channel = 7
-    rf_freq = 434.550 + rf_channel * 0.1
-
-    # Attempt setting up RFM9x Module
-    try:
-        rfm9x = adafruit_rfm9x.RFM9x(spi, CS, RST, rf_freq)
-        rfm9x.tx_power = 23
-        print('RFM9x successfully set up!')
-
-    except RuntimeError as error:
-        print('Error in setting up RFM9x... check wiring.')
-        return False
-        
+def transmit():        
 
     print("Transmitting...")
     SECOND_NS = 1_000_000_000
@@ -63,7 +45,19 @@ def transmit():
             last_sample_T = this_sample_T
 
 def receiveLoop():
-    pass
+    print("Receiving...")
+    f = open("rf_receive.log", "w+")
+    f.write(f"Collection log started: {datetime.datetime.now()}\n")
+    
+    while True:
+        # Receive
+        rx_packet = rfm9x.receive()
+        if rx_packet:
+            receive_time = time.time_ns()
+            rx_data = str(rx_packet, "utf-8")
+
+            f.write(f'{receive_time} <- {rx_data}\n')
+            print(f'{receive_time} <- {rx_data}')
 
 def main():
     if (len(sys.argv) != 2):
@@ -79,4 +73,21 @@ def main():
         return False
 
 if __name__ == '__main__':
-    main()
+    # Initialize radio
+    spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
+    RST = DigitalInOut(board.D25)
+    CS = DigitalInOut(board.CE1)
+
+    # Configure radio frequencies
+    rf_channel = 7
+    rf_freq = 434.550 + rf_channel * 0.1
+
+    # Attempt setting up RFM9x Module
+    try:
+        rfm9x = adafruit_rfm9x.RFM9x(spi, CS, RST, rf_freq)
+        rfm9x.tx_power = 23
+        print('RFM9x successfully set up!')
+        main()
+
+    except RuntimeError as error:
+        print('Error in setting up RFM9x... check wiring.')
