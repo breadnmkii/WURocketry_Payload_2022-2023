02aad6f819fe0397cb325c633a8e574461a641eb
breadnmkii <b.z.yang@wustl.edu>
2023-01-12 22:13:29 -0600

    main avionics work

--- dev/Avionics/bno055.py
+++ dev/Avionics/bno055.py
@@ -14,19 +14,23 @@ if __name__ == '__main__':
     bno055 = a_bno.BNO055_I2C(i2c)
 
     ### IMU Configuration
-    bno055.mode = a_bno.NDOF_MODE
+    # Change to configuration mode
+    bno055.mode = a_bno.CONFIG_MODE
     ## Accelerometer Config
     # bno055.accel_mode = a_bno.ACCEL_NORMAL_MODE
-    bno055.accel_range = a_bno.ACCEL_16G
+    # bno055.accel_range = a_bno.ACCEL_16G
     # bno055.accel_bandwidth = a_bno.ACCEL_500HZ
     ## Gyroscope Config
     # bno055.gyro_mode = a_bno.GYRO_NORMAL_MODE
     # bno055.gyro_range = a_bno.GYRO_2000_DPS
     # bno055.gyro_bandwidth = a_bno.GYRO_523HZ
     ## Magnetometer Config
-    bno055.magnet_power_mode = a_bno.MAGNET_FORCEMODE_MODE
     # bno055.magnet_operation_mode = a_bno.MAGNET_ACCURACY_MODE
+    # bno055.magnet_mode = a_bno.MAGNET_FORCEMODE_MODE
     # bno055.magnet_rate = a_bno.MAGNET_30HZ
+
+    # Revert back to operation mode    
+    bno055.mode = a_bno.AMG_MODE
 
     SECOND_NS = 1_000_000_000
     NUM_READINGS = 25
@@ -41,7 +45,7 @@ if __name__ == '__main__':
         if (this_sample_T >= last_sample_T + DELTA_T):
 
             if (bno055.calibrated):
-                # Read sensor data
+                # Read bno055 data
                 print(f"time:{this_sample_T-start_sample_T}\tgyro:{bno055.gyro}\taccl:{bno055.acceleration}\tmagn:{bno055.magnetic}")
                 NUM_READINGS -= 1
             else:
