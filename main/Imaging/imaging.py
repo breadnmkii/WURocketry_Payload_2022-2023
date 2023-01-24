a66861881bcb2a6bdafc475682da0ff7e2c131a0
Autumn Xu <autumn@wustl.edu>
2023-01-23 20:25:49 -0600

    some documentation

--- main/Imaging/imaging.py
+++ main/Imaging/imaging.py
@@ -3,7 +3,6 @@ import numpy as np
 import picamera
 from time import sleep
 
-
 #create object for PiCamera class
 camera = picamera.PiCamera()
 #set resolution
@@ -11,28 +10,35 @@ camera.resolution = (1024, 768)
 camera.brightness = 60
 # write each function for each RAFCO command that is camera related 
 
+
+# modify input, outputs 
+# no input -> find the photo most recently taken, then perform modifications as needed 
 '''
+D4: change camera mode from color to grayscale
 input: image, 
 return its grayscale version
 '''
-
 def greyscale2rgb(img):
+    camera.color_effects = (128,128) # turn camera to black and white
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     return gray
 
 '''
+F6: rotate camera 180 degrees
 input: image, 
 return its 180 degree rotation
 '''
-
 def rotate(img):
     rotated = cv2.rotate(img, cv2.ROTATE_180)
     return rotated 
 
+# E5: change camera mode back from grayscale to color 
+# H8: remove all filters 
 def get_original():
     return img
 
 '''
+G7: special effects filter to the image 
 input: image
 output: image with color space switched from RGB to BGR
 '''
@@ -40,6 +46,7 @@ def rgb2bgr(img):
     rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     return rgb
 
+# C3: take picture
 def take_picture():
     #create object for PiCamera class
 	camera = picamera.PiCamera()
@@ -65,8 +72,6 @@ if __name__ == '__main__':
     #cv2.destroyAllWindows() 
     
 
-# D4 E5 change global camera mode
-# global variable???
 # https://www.nasa.gov/sites/default/files/atoms/files/2023_slhandbook_508.pdf
 
 # add timestamp: format with date/time
