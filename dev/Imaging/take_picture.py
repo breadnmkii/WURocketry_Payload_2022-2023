fa9be30a2d7262445a6031e4337b0ed5bc5aa84c
Autumn Xu <autumn@wustl.edu>
2023-01-29 14:00:35 -0600

    working on time

--- dev/Imaging/take_picture.py
+++ dev/Imaging/take_picture.py
@@ -10,24 +10,26 @@ sys.path.append('paste the copied path here')
 import picamera
 from time import sleep
 from datetime import datetime
-from pytz import timezone
+#from pytz import timezone
+import pytz
 
 #create object for PiCamera class
 camera = picamera.PiCamera()
 #set resolution
 camera.resolution = (1024, 768)
 camera.brightness = 60
-central = timezone('US/Central')
-camera.start_preview()
+#central = timezone('US/Central')
+#camera.start_preview()
 now = datetime.now()
-utc = timezone('UTC')
+#utc = timezone('UTC')
+timezone = pytz.timezone("America/Central")
 now = timezone.localize(now)
-print(current_time)
+print(now)
 current_time = now.strftime("%H:%M:%S %Z")
-published_time = datetime.strptime(current_time, '%H:%M:%S %Z')
-published_gmt = published_time.replace(tzinfo=utc)
-published_cst = published_gmt.astimezone(central)
-actual_time_published = published_cst.strftime('%I:%M:%S %p %Z')
+#published_time = datetime.strptime(current_time, '%H:%M:%S %Z')
+#published_gmt = published_time.replace(tzinfo=utc)
+#published_cst = published_gmt.astimezone(central)
+actual_time_published = current_time.strftime('%I:%M:%S %p %Z')
 #add text on image
 camera.annotate_text = actual_time_published
 sleep(5)
