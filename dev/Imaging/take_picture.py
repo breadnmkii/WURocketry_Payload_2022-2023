2fb14f8117d1ce7fe830cef217dda68bcf5dfb60
Autumn Xu <autumn@wustl.edu>
2023-01-29 13:49:42 -0600

    tweaking time

--- dev/Imaging/take_picture.py
+++ dev/Imaging/take_picture.py
@@ -10,17 +10,24 @@ sys.path.append('paste the copied path here')
 import picamera
 from time import sleep
 from datetime import datetime
+from pytz import timezone
 
 #create object for PiCamera class
 camera = picamera.PiCamera()
 #set resolution
 camera.resolution = (1024, 768)
 camera.brightness = 60
+central = timezone('US/Central')
 camera.start_preview()
 now = datetime.now()
-current_time = now.strftime("%H:%M:%S")
+current_time = now.strftime("%H:%M:%S %Z")
+published_time = datetime.strptime(current_time, '%H:%M:%S %Z')
+utc = timezone('UTC')
+published_gmt = published_time.replace(tzinfo=utc)
+published_cst = published_gmt.astimezone(central)
+actual_time_published = published_cst.strftime('%I:%M:%S %p %Z')
 #add text on image
-camera.annotate_text = current_time
+camera.annotate_text = actual_time_published
 sleep(5)
 #store image
 
