058227532f0f64b729822fd5ffd0af93ee659078
Autumn Xu <autumn@wustl.edu>
2023-01-30 19:44:26 -0600

    cleaning

--- dev/Imaging/take_picture.py
+++ dev/Imaging/take_picture.py
@@ -24,28 +24,21 @@ camera.brightness = 60
 
 # Get the timezone object for Chicago
 tz_cst = pytz.timezone('America/Chicago') 
-
-# Get the current time in New York
+# Get the current time in CST
 datetime_cst = datetime.now(tz_cst)
-
+# Format the time as a string 
 local_time = datetime_cst.strftime("%H:%M:%S")
-# Format the time as a string and print it
-print("CST time:", local_time, type(local_time))
-
+#print("CST time:", local_time, type(local_time))
+# get date 
 today_date = date.today()
-print("Today's date:", today_date, type(today_date))
-
+#print("Today's date:", today_date, type(today_date))
 annotation = str(today_date)+'_'+local_time
 print("full annotation: ", annotation)
-
-
 #camera.start_preview()
-
 camera.annotate_text = annotation
 sleep(5)
 #store image
-
-camera.capture(str(annotation)+'.jpeg')
+camera.capture(annotation+'.jpeg')
 # camera.stop_preview()
 
 def camera_time():
