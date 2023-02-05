3d6f3d3f788a922c540ee229af1cbd7b46a27b61
jiwenofu <c.iwenofu@wustl.edu>
2023-02-05 13:10:37 -0600

    FSM timing work

---
+++ main/Radio/telemenetry.py
@@ -0,0 +1,46 @@
+#Working on RFM96w, reference it as RFM9x
+
+import time
+import datetime     # for testing
+import busio
+import board
+import rfmconfig
+from digitalio import DigitalInOut
+
+"""
+ Pins
+   19 - MOSI
+   21 - MISO
+   22 - RST
+   23 - SCK
+   26 - CS
+   
+"""
+
+# Configure RFM9x LoRa Radio
+CS = DigitalInOut(board.CE1)
+RESET = DigitalInOut(board.D25)
+spi = busio.SPI(board.SCK, MOSI=board.MOSI, MISO=board.MISO)
+
+# Configure radio frequencies
+rf_channel = 7
+rf_freq = 434.550 + rf_channel * 0.1
+
+# Global RFM96w object to send/receieve data
+RFM96W = rfmconfig.config_RFM96w()
+f = open("rf_test.txt", "w+")
+f.write(f"Collection started: {datetime.datetime.now()}\n")
+
+def transmitData(str):
+    # RX
+    tx_packet =  bytes(f'{str}\r\n', 'utf-8')
+    RFM96W.send(tx_packet)
+    print(f'Sent -> {tx_packet}')
+
+def recieveData():
+    rx_packet = RFM96W.recieve()
+    if rx_packet:
+        receive_time = time.time_ns()
+        rx_data = str(rx_packet, "utf-8")
+        f.write(f'{receive_time} <- {rx_data}\n')
+        print(f'{receive_time} <- {rx_data}') 
