63e7735add21155cce308c28c05c27d27ab4cc86
Autumn Xu <autumn@wustl.edu>
2023-02-05 14:18:19 -0600

    trying

--- main/Imaging/imaging.py
+++ main/Imaging/imaging.py
@@ -34,9 +34,12 @@ def rotate():
     list_of_files = glob.glob('./image_results/*.jpg') # * means all if need specific format then *.csv
     latest_file = max(list_of_files, key=os.path.getctime)
     print(latest_file)
+    if (not img):
+        print("cannot read image right now")
     img = cv2.imread(latest_file)
+    
     rotated = cv2.rotate(img, cv2.ROTATE_180)
-    file_path = str('./image_results/'+latest_file+'_rotated')
+    file_path = str(latest_file+'_rotated')
     cv2.imwrite(file_path, rotated)
     return rotated 
 
@@ -72,6 +75,7 @@ def rgb2bgr(img):
     cv2.imwrite(file_path, rgb)
     return rgb
 
+# move picture taken to image_results
 # C3: take picture
 def take_picture():
     '''
