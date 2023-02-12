e9d743301705928226ddb0e814a7856fc711dc50
Autumn Xu <autumn@wustl.edu>
2023-02-12 11:42:28 -0600

    fixing out of resources error

--- main/Imaging/config.py
+++ main/Imaging/config.py
@@ -1,9 +1,11 @@
 import picamera
+''' this seems to cause out of resources error
 #create object for PiCamera class
 camera = picamera.PiCamera()
 #set resolution
 camera.resolution = (1024, 768)
 camera.brightness = 60
+'''
 print("in config.py")
 
 
