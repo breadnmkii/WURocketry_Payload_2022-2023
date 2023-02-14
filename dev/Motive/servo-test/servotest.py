ccf13af48c1f00f0c186eff5591f7daaf90f3abc
loganfarrow <89869123+loganfarrow@users.noreply.github.com>
2023-02-13 18:12:21 -0600

    Update servotest.py
    servo test update

--- dev/Motive/servo-test/servotest.py
+++ dev/Motive/servo-test/servotest.py
@@ -1,47 +1,150 @@
-# Import libraries
-import RPi.GPIO as GPIO
+import lib_para_360_servo
+import pigpio
 import time
+import math
+import atexit
+    
 
-# Set GPIO numbering mode
-GPIO.setmode(GPIO.BOARD)
 
-# Set pin 11 as an output, and set servo1 as pin 11 as PWM
-GPIO.setup(11,GPIO.OUT)
-servo1 = GPIO.PWM(11,10) # Note 11 is pin, 50 = 50Hz pulse
+# Calculate angular position (degrees)
+# ang = ((read_dc - dc_min) * 360) / (dc_max - dc_min + 1) 
+def get_angpos_helper(read_dc):
+    # Constants
+   DC_MIN = 3.185 # / 10
+   DC_MAX = 99.19 # / 10
 
-#start PWM running, but with value of 0 (pulse off)
-servo1.start(0)
-print ("Waiting for 1 seconds")
-time.sleep(1)
-print("Running");
+   return ((read_dc - DC_MIN) * 360) / (DC_MAX - DC_MIN + 1)
 
-# Sweep duty values from 0 to 100 (1 sec)
 
-for duty in range(40,60,1):
-    print(f"Duty cycle of {duty:.2f}%")
-    servo1.ChangeDutyCycle(duty/100)
-    time.sleep(2)
+#set a custom angle position
+def set_angpos(servo, angle):
+    curr_pos = get_angpos()
+    angle = angle % 360
 
-'''
-print("0.1")
-servo1.ChangeDutyCycle(0.1)
-time.sleep(1)
-print("0.3")
-servo1.ChangeDutyCycle(0.3)
-time.sleep(1)
-print("0.5")
-servo1.ChangeDutyCycle(0.5)
-time.sleep(1)
-print("0.7")
-servo1.ChangeDutyCycle(0.7)
-time.sleep(1)
-print("1")
-servo.ChangeDutyCycle(1)
-time.sleep(1)
-'''
+    if angle < curr_pos:
+        servo.set_speed(-0.15)
+    else:
+        servo.set_speed(0.09)
 
-#Clean things up at the end
-servo1.stop()
-GPIO.cleanup()
-print ("Goodbye")
+    while (curr_pos > angle+margin or curr_pos < angle-margin):
+        curr_pos = get_angpos()
+    servo.stop()
+
+
+    
+#return the current angular position
+def get_angpos():
+    position = round((get_angpos_helper(reader.read()/10)), 2)
+
+    print("Current Position ", position)
+
+    return position
+
+
+#set angular position to zero    
+def set_zero(servo):
+    set_angpos(servo, 0)
+    print("Set 360 Position to Zero Sucessfully")
+    
+    
+def left_60(servo):
+    current = get_angpos()
+    current = current - 60
+    if current < 0:
+        current = current + 360
+    print("Moving Left!")
+    set_angpos(servo, current)
+    
+
+def right_60(servo):
+    current = get_angpos()
+    current = current + 60
+    print("Moving Right!")
+    set_angpos(servo, current)
+
+def exit_handler():
+    
+    servo.stop()
+    pi.stop()
+    print('Finished!')
+  
+
+if __name__ == '__main__':
+
+    #Define GPIO pins that we will read and write to (Using GPIO numbers NOT pin numbers)
+    WHITE_CABLE = 24
+    YELLOW_CABLE = 23
+
+    #define margin of error
+    margin = 2
+
+
+    #init pigpio to access GPIO pins with PWM
+    pi = pigpio.pi()
+
+    #init servo and servo position reader from lib_para_360_servo
+    servo = lib_para_360_servo.write_pwm(pi = pi, gpio = YELLOW_CABLE)
+    reader = lib_para_360_servo.read_pwm(pi = pi, gpio = WHITE_CABLE)
+
+    #create a handler to run exit requirements
+    atexit.register(exit_handler)
+
+    print("Pi Servo Controller | Enter speed range:")
+    while (True):
+        control = input(" : ")
+        if (control == "exit"):
+            servo.stop()
+            break
+        servo.set_speed(float(control))
+
+
+
+
+    # Buffer time for initializing library servo
+    # time.sleep(1)
+    # servo.stop()
+    
+    # set_zero(servo)
+    # time.sleep(1)
+    # right_60(servo)
+    # time.sleep(1)
+    # right_60(servo)
+    # time.sleep(1)
+    # right_60(servo)
+    # time.sleep(1)
+    # right_60(servo)
+    # time.sleep(1)
+    # right_60(servo)
+    # time.sleep(1)
+    # right_60(servo)
+    # time.sleep(1)
+    # right_60(servo)
+    # time.sleep(1)
+    # right_60(servo)
+    # time.sleep(1)
+    # right_60(servo)
+    # time.sleep(1)
+    # right_60(servo)
+    # time.sleep(1)
+    # left_60(servo)
+    # time.sleep(1)
+    # left_60(servo)
+    # time.sleep(1)
+    # left_60(servo)
+    # time.sleep(1)
+    # left_60(servo)
+    # time.sleep(1)
+    # left_60(servo)
+    # time.sleep(1)
+    # left_60(servo)
+    # time.sleep(1)
+    # left_60(servo)
+    # time.sleep(1)
+    
+    #while(1):
+     #   val = int(input("Enter your value: "))
+        
+     #   set_angpos(servo, val)
+     #   print(val)
+      #  atexit.register(exit_handler)
 
