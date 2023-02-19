1b8446c39c54ff7888632a81ee8573b0a4c9d53e
breadnmkii <b.z.yang@wustl.edu>
2023-02-19 20:25:16 +0000

    motor work

--- dev/Motive/motor-test/motorhat.py
+++ dev/Motive/motor-test/motorhat.py
@@ -1,15 +1,15 @@
 import time
 import board
-import numpy as np
+# import numpy as np
 from adafruit_motorkit import MotorKit
 
-def throttleLoop(hat, min, max, step, reverse=False):
-    for val in np.arange(min, max, step):
-        if reverse:
-            val *= -1
-        print("Throttle @", val)
-        hat.motor1.throttle = val
-        time.sleep(3)
+# def throttleLoop(hat, min, max, step, reverse=False):
+#     for val in np.arange(min, max, step):
+#         if reverse:
+#             val *= -1
+#         print("Throttle @", val)
+#         hat.motor1.throttle = val
+#         time.sleep(3)
 
 
 if __name__ == '__main__':
@@ -18,10 +18,13 @@ if __name__ == '__main__':
     hat = MotorKit(i2c = board.I2C())
 
     print("Starting...")
-    time.sleep(15)
-
-    throttleLoop(hat, 0, 1.1, 0.1)
-
+    time.sleep(5)
+    print("Running")
+    #throttleLoop(hat, 0, 1.1, 0.1)
+    hat.motor1.throttle = 1
+    time.sleep(5)
+    print("Done!")
+    hat.motor1.throttle = 0
     """
 
     print("25% Speed")
--- dev/Motive/servo-test/360_servo.py
+++ dev/Motive/servo-test/360_servo.py
@@ -7,12 +7,15 @@ import atexit
 #Define GPIO pins that we will read and write to (Using GPIO numbers NOT pin numbers)
 WHITE_CABLE_SIGNAL = 23
 YELLOW_CABLE_FEEDBACK = 24
-MARGIN = 3
-COUNT = 0
+
+#Define the margin of error
+MARGIN = 2
+
+#global current angular position variable
+current = 0
 
 
 # Calculate angular position (degrees)
-# ang = ((read_dc - dc_min) * 360) / (dc_max - dc_min + 1) 
 def get_angpos_helper(read_dc):
     # Constants
    DC_MIN = 3.185 # / 10
@@ -22,17 +25,21 @@ def get_angpos_helper(read_dc):
 
 
 #set a custom angle position
-def set_angpos(servo, angle):
-    curr_pos = get_angpos()
-    angle = angle % 360
-
-    if angle < curr_pos:
+def set_angpos(servo, moveto_angle):
+    
+    global current
+    
+    if moveto_angle < current:
         servo.set_speed(-0.2)   # @6v -0.15
     else:
         servo.set_speed(0.1)    # @6v 0.09
 
-    while (curr_pos > angle+MARGIN or curr_pos < angle-MARGIN):
+
+    curr_pos = get_angpos()
+    while (curr_pos > moveto_angle+MARGIN or curr_pos < moveto_angle-MARGIN):
         curr_pos = get_angpos()
+    
+    current = moveto_angle
     servo.stop()
 
 
@@ -40,11 +47,7 @@ def set_angpos(servo, angle):
 #return the current angular position
 def get_angpos():
     position = round((get_angpos_helper(reader.read()/10)), 2)
-    global COUNT
 
-    #if COUNT % 20000 == 0:
-       # print("Current Position ", position)
-   # COUNT +=1
 
     return position
 
@@ -54,23 +57,41 @@ def set_zero(servo):
     set_angpos(servo, 0)
     print("Set 360 Position to Zero Sucessfully")
     
+
+
+
+
+
+
     
 def left_60(servo):
-    current = get_angpos()
-    current = current - 60
-    if current < 0:
-        current = current + 360
-    current = current % 360
-    print("Moving Left! to ", current)
-    set_angpos(servo, current)
+    global current
+
+    moveto_angle = current - 60
+
+    if moveto_angle < 0:
+        moveto_angle = moveto_angle + 360
     
+    print("Moving Left! to ", moveto_angle)
+    set_angpos(servo, moveto_angle)
+
+
+
+
+
 
 def right_60(servo):
-    current = get_angpos()
-    current = current + 60
-    current = current % 360
-    print("Moving Right! to ", current)
-    set_angpos(servo, current)
+    global current
+
+    moveto_angle = current + 60
+    moveto_angle = moveto_angle % 360
+
+    print("Moving Right! to ", moveto_angle)
+    set_angpos(servo, moveto_angle)
+
+
+
+
 
 def exit_handler():
     
@@ -78,6 +99,11 @@ def exit_handler():
     pi.stop()
     print('Finished!')
   
+
+
+
+
+
 
 if __name__ == '__main__':
 
@@ -92,15 +118,6 @@ if __name__ == '__main__':
     #create a handler to run exit requirements
     atexit.register(exit_handler)
 
-   # print("Pi Servo Controller | Enter speed range:")
-   # while (True):
-   #     control = input(" : ")
-   #     if (control == "exit"):
-   #         servo.stop()
-   #         break
-   #     servo.set_speed(float(control))
-
-
 
     #Buffer time for initializing library servo
     time.sleep(2)
@@ -109,11 +126,12 @@ if __name__ == '__main__':
 
     time.sleep(1)
     
+    #if(get_angpos() > 5):
     set_zero(servo)
 
     time.sleep(1)
     print("60")
-    right_60(servo)
+    left_60(servo)
     
     time.sleep(1)
     print("0")
@@ -123,8 +141,33 @@ if __name__ == '__main__':
     left_60(servo)
 
     time.sleep(1)
+    left_60(servo)
+
+    time.sleep(1)
+    left_60(servo)
+
+    time.sleep(1)
+    left_60(servo)
+
+    time.sleep(1)
+    left_60(servo)
+    
+    time.sleep(1)
+    print("60")
+    right_60(servo)
+    
+    time.sleep(1)
+    print("0")
+    right_60(servo)
+    
+    time.sleep(1)
     right_60(servo)
 
     time.sleep(1)
     right_60(servo)
-    
+
+    time.sleep(1)
+    right_60(servo)
+
+    time.sleep(1)
+    right_60(servo)    
