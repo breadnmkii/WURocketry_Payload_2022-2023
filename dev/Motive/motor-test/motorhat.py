a900a8935ff73f9505fa8c89839fd9cc9f9cd69a
jiwenofu <c.iwenofu@wustl.edu>
2023-02-20 18:10:47 -0600

    motorhat

--- dev/Motive/motor-test/motorhat.py
+++ dev/Motive/motor-test/motorhat.py
@@ -1,15 +1,15 @@
 import time
 import board
-import numpy as np
+# import numpy as np
 from adafruit_motorkit import MotorKit
 
-def throttleLoop(hat, min, max, step, reverse=False):
-    for val in np.arange(min, max, step):
-        if reverse:
-            val *= -1
-        print("Throttle @", val)
-        hat.motor1.throttle = val
-        time.sleep(3)
+# def throttleLoop(hat, min, max, step, reverse=False):
+#     for val in np.arange(min, max, step):
+#         if reverse:
+#             val *= -1
+#         print("Throttle @", val)
+#         hat.motor1.throttle = val
+#         time.sleep(3)
 
 
 if __name__ == '__main__':
@@ -20,8 +20,10 @@ if __name__ == '__main__':
     print("Starting...")
     time.sleep(15)
 
-    throttleLoop(hat, 0, 1.1, 0.1)
-
+    #throttleLoop(hat, 0, 1.1, 0.1)
+    hat.motor1.throttle = 1
+    time.sleep(5)
+    hat.motor1.throttle = 0
     """
 
     print("25% Speed")
