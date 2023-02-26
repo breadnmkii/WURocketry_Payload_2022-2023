75e3a288062133df5c358c02ba817cb7618cf060
loganfarrow <89869123+loganfarrow@users.noreply.github.com>
2023-02-26 12:35:35 -0600

    Update rf_decode.py

--- dev/Radio/APRS/rf_decode.py
+++ dev/Radio/APRS/rf_decode.py
@@ -1,16 +1,19 @@
-#run rtl_recieve.sh first
+import subprocess
+import time
 
-#open text file in read mode
-text_file = open("data.txt", "r")
- 
-#read whole file to a string
-data = text_file.read()
- 
-#close file
-text_file.close()
+# Launch the subprocess
+with open("data", "w") as file:
+    process = subprocess.Popen(["rtl_fm", "-f", "144.390M", "-s", "22050"], 
+                               stdout=subprocess.PIPE, 
+                               stderr=subprocess.PIPE)
+    subprocess.Popen(["multimon-ng", "-t", "raw", "-a", "AFSK1200", "-f", "alpha", "/dev/stdin"], 
+                     stdin=process.stdout, 
+                     stdout=file, 
+                     stderr=subprocess.PIPE)
 
-#make string into a list
-transmission = data.split()
-
-#show us the list
-print(transmission)
+# Read the data in the main Python code
+while True:
+    with open("data") as file:
+        data = file.readlines()
+    print(data)
+    time.sleep(1)
