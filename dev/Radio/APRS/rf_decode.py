86e8e04564621ed04c420e7c4a8c3c3d6006a5f3
breadnmkii <b.z.yang@wustl.edu>
2023-02-26 19:16:57 +0000

    work

--- dev/Radio/APRS/rf_decode.py
+++ dev/Radio/APRS/rf_decode.py
@@ -1,19 +1,13 @@
 import subprocess
 import time
 
-# Launch the subprocess
-with open("data", "w") as file:
-    process = subprocess.Popen(["rtl_fm", "-f", "144.390M", "-s", "22050"], 
-                               stdout=subprocess.PIPE, 
-                               stderr=subprocess.PIPE)
-    subprocess.Popen(["multimon-ng", "-t", "raw", "-a", "AFSK1200", "-f", "alpha", "/dev/stdin"], 
-                     stdin=process.stdout, 
-                     stdout=file, 
-                     stderr=subprocess.PIPE)
-
-# Read the data in the main Python code
-while True:
-    with open("data") as file:
-        data = file.readlines()
-    print(data)
-    time.sleep(1)
+def begin_APRS_recieve():
+    with open("data", "w") as file:
+        process = subprocess.Popen(["rtl_fm", "-f", "144.390M", "-s", "22050"], 
+                                   stdout=subprocess.PIPE, 
+                                   stderr=subprocess.PIPE)
+        subprocess.Popen(["multimon-ng", "-t", "raw", "-a", "AFSK1200", "-f", "alpha", "/dev/stdin"], 
+                         stdin=process.stdout, 
+                         stdout=file, 
+                         stderr=subprocess.PIPE)
+    return process
