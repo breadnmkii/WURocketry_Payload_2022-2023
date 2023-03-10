f7f5f876cae04df3871934e8bfe05505a74a5117
Autumn Xu <autumn@wustl.edu>
2023-03-10 14:40:23 -0600

    testing buffer

--- main/Avionics/sensing.py
+++ main/Avionics/sensing.py
@@ -210,7 +210,7 @@ def average_window(list, window, pointer):
     
 def average_sum_abs_range(list, least_recent, most_recent):
     of_interest = list[least_recent:most_recent+1]
-    values = [abs(x) for x in of_interest if x is not None]
+    values = [x for x in of_interest if x is not None]
     if len(values) == 0:
         return 0
     return sum(values) / len(values)
