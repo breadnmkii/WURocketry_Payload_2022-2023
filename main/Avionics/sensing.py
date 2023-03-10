f209bd6f34a35000181cffb1b0125575135abf48
Autumn Xu <autumn@wustl.edu>
2023-03-10 15:53:51 -0600

    testing buffer

--- main/Avionics/sensing.py
+++ main/Avionics/sensing.py
@@ -56,6 +56,7 @@ def read_bno():
     quat = bno.quaternion
     global bno_pointer
     global euler_orient_pointer
+    global linear_acc_pointer
     #bno_pointer = bno_pointer % BNO_BUFFER_LEN
     euler_orient_pointer = euler_orient_pointer % BNO_BUFFER_LEN
     linear_acc_pointer = linear_acc_pointer % BNO_BUFFER_LEN
