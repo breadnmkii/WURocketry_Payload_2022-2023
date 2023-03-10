dae280480d70c6144e8c5f9c1e16b150cf844f6a
Autumn Xu <autumn@wustl.edu>
2023-03-10 16:57:13 -0600

    testing buffer

--- main/Avionics/sensing.py
+++ main/Avionics/sensing.py
@@ -305,10 +305,10 @@ functionality: detect whether the payload is moving up, or moving down or
 '''
 def altitude_status(altitude_accumulator, pressure_accumulator):
     rolling_window = 50
-    descent_altitude = -2
-    ascent_altitude =  2
-    descent_pressure = 2
-    ascent_pressure =  -2
+    descent_altitude = -2 # HOW TO TEST THRESHOLD
+    ascent_altitude =  2 # HOW TO TEST THRESHOLD
+    descent_pressure = 2 # HOW TO TEST THRESHOLD
+    ascent_pressure =  -2 # HOW TO TEST THRESHOLD
     if (differential_window(altitude_accumulator, rolling_window) < descent_altitude and differential_window(pressure_accumulator, rolling_window) > descent_pressure):
         print('BMP -- payload is moving up')
         return 'up'
