444e1e684fb1090bc9d66fabf0d65b39e8753a61
breadnmkii <b.z.yang@wustl.edu>
2023-03-20 13:31:39 -0500

    enum revision and telemetry packets

--- main/enums.py
+++ main/enums.py
@@ -28,50 +28,47 @@ class Deployed(Enum):
     DEPLOYED = 1
 
 class Warn_Heat(Enum):
-    NORMAL = 0
+    NOMINAL = 0
     WARNING = 1
 
 class Warn_Camera(Enum):
-    NORMAL = 0
+    NOMINAL = 0
     WARNING = 1
-
 
 class Warn_Avionics(Enum):
-    NORMAL = 0
-    WARNING = 1
+    NOMINAL = 0
+    WARNING_BNO = 1
+    WARNING_BMP = 2
+    WARNING_SYS = 3 # entire system (both bno and bmp warning)
 
 class Warn_Motive(Enum):
-    # Logan
-    pass
+    NOMINAL = 0
+    WARNING = 1
 
-
-class System_Flags(Enum):
-    STAGE_INFO = (Stage.PRELAUNCH, Stage.MIDAIR, Stage.LANDED)
-    MOVEMENT = (Movement.NOT_MOVING, Movement.MOVING, Movement.CONFLICTING_DECISION)
-    FLIGHT_DIRECTION = (Flight_Direction.MOVING_UP, Flight_Direction.MOVING_DOWN, Flight_Direction.INDETERMINENT)
-    VERTICALITY = (Verticality.NOT_UPRIGHT, Verticality.UPRIGHT)
-    # John
-    SEPARATED = (Separated.SEPARATED, Separated.NOT_SEPARATED)
-    # Logan
-    DEPLOYED = (Deployed.DEPLOYED, Deployed.NOT_DEPLOYED)
-
-    WARN_HEAT = (Warn_Heat.NORMAL, Warn_Heat.WARNING_HEAT)
-    WARN_CAMERA = (Warn_Camera.NORMAL, Warn_Camera.WARNING_CAMERA)
-    WARN_AVIONICS = (Warn_Avionics.NORMAL, Warn_Avionics.WARNING_BMP, Warn_Avionics.WARNING_BNO)
-    # Logan
-    WARN_MOTIVE = None
+class System_Flags():
+    # STAGE_INFO = (Stage.PRELAUNCH, Stage.MIDAIR, Stage.LANDED)
+    # MOVEMENT = (Movement.NOT_MOVING, Movement.MOVING, Movement.CONFLICTING_DECISION)
+    # FLIGHT_DIRECTION = (Flight_Direction.MOVING_UP, Flight_Direction.MOVING_DOWN, Flight_Direction.INDETERMINENT)
+    # VERTICALITY = (Verticality.NOT_UPRIGHT, Verticality.UPRIGHT)
+    # SEPARATED = (Separated.SEPARATED, Separated.NOT_SEPARATED)
+    # DEPLOYED = (Deployed.DEPLOYED, Deployed.NOT_DEPLOYED)
+    # WARN_HEAT = (Warn_Heat.NOMINAL, Warn_Heat.WARNING)
+    # WARN_CAMERA = (Warn_Camera.NOMINAL, Warn_Camera.WARNING)
+    # WARN_AVIONICS = (Warn_Avionics.NOMINAL, Warn_Avionics.WARNING_BMP, Warn_Avionics.WARNING_BNO, Warn_Avionics.WARNING_SYS)
+    # WARN_MOTIVE = (Warn_Motive.NOMINAL, Warn_Motive.WARNING)
 
     def __init__(self, STAGE_INFO, MOVEMENT, FLIGHT_DIRECTION, VERTICALITY, SEPARATED, DEPLOYED, HEAT, CAMERA_MODULE, AVIONICS, MOTIVE):
         self.STAGE_INFO = STAGE_INFO
         self.MOVEMENT = MOVEMENT
         self.FLIGHT_DIRECTION = FLIGHT_DIRECTION
         self.VERTICALITY = VERTICALITY
-        # John
         self.SEPARATED = SEPARATED
-        # Logan
         self.DEPLOYED = DEPLOYED
         self.WARN_HEAT = HEAT
         self.WARN_CAMERA = CAMERA_MODULE
         self.WARN_AVIONICS = AVIONICS 
-        # Logan
         self.WARN_MOTIVE = MOTIVE
+    
+    def get_str(self):
+        flags = (str(self.STAGE_INFO), str(self.MOVEMENT), str(self.FLIGHT_DIRECTION), str(self.VERTICALITY), str(self.SEPARATED), str(self.DEPLOYED), str(self.WARN_HEAT), str(self.WARN_CAMERA), str(self.WARN_AVIONICS), str(self.WARN_MOTIVE))
+        return flags
