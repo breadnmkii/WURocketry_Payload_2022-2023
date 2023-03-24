ca3d11c4e75f1464cbbc280f06e0292d28f5f1c9
Autumn Xu <autumn@wustl.edu>
2023-03-24 15:01:10 -0500

    also avionics

--- main/main.py
+++ main/main.py
@@ -7,7 +7,7 @@
 # from Imaging import config as imaging_config
 # from Motive import config as motive_config
 
-# from Avionics import sensing
+from Avionics import sensing
 from Control import fsm
 # from Motive import camarm
 from Radio import APRS
@@ -325,6 +325,7 @@ def test_main():
         currentState = fsmUpdate[0]
         currRAFCO_S_idx = fsmUpdate[1]
         currRAFCO_idx = fsmUpdate[2]
+
 
 if __name__ == '__main__':
     test_main()
