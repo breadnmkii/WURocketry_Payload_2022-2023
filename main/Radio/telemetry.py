4996bc226cb83ade4587146883b550a4da448af1
Autumn Xu <autumn@wustl.edu>
2023-03-27 21:42:13 -0500

    minor fix

--- main/Radio/telemetry.py
+++ main/Radio/telemetry.py
@@ -46,7 +46,7 @@ def recieveData():
     if rx_packet:
         receive_time = time.time_ns()
         rx_data = str(rx_packet, "utf-8")
-        rx_data = int(rx_data.replace("|", "").replace(" ", ""))
+        rx_data = int(rx_data.replace("|", "").replace(" ", "").replace(",", ""))
         encoding = enums.Stage(rx_data[0])+enums.Movement(rx_data[1])+enums.Flight_Direction(rx_data[2])+enums.Verticality(rx_data[3])+enums.Separated(rx_data[4])+enums.Deployed(rx_data[5])+enums.Warn_Heat(rx_data[6])+enums.Warn_Camera(rx_data[7])+enums.Warn_Avionics(rx_data[8])+enums.Warn_Motive(rx_data[9])
         
         f.write(f'{receive_time} <- {encoding}\n')
