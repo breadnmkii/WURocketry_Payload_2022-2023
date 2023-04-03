345ee6fcbe93274f978718e9c7dabe6436bf7da2
Autumn Xu <autumn@wustl.edu>
2023-04-03 18:58:33 -0500

    testing dev

---
+++ dev/Imaging/config.py
@@ -0,0 +1,19 @@
+import picamera
+''' this seems to cause out of resources error
+#create object for PiCamera class
+camera = picamera.PiCamera()
+#set resolution
+camera.resolution = (1024, 768)
+camera.brightness = 60
+'''
+
+def init_camera():
+    #create object for PiCamera class
+    camera = picamera.PiCamera()
+    #set resolution
+    camera.resolution = (1024, 768)
+    camera.brightness = 60
+    camera.annotate_background = picamera.Color('black', alpha=128)
+    print("....camera mode: ", camera.sensor_mode)
+    print("...setting camera resolution: ", camera.resolution)
+    return camera
---
+++ dev/Imaging/test_imaging.py
@@ -0,0 +1,153 @@
+# import cv2 # stay away from opencv that can not be downloaded onto the pi
+#import numpy as np
+from PIL import Image
+from time import sleep
+from datetime import datetime
+from pytz import timezone
+#from PIL import Image
+from datetime import date
+import config
+import glob
+import os
+
+print("Initializing camera in imaging.py")
+camera = config.init_camera()
+# PiCamera hardware fault
+
+if (camera == None):
+    print("Camera could not be initialized!")
+
+ 
+# modify input, outputs 
+# no input -> find the photo most recently taken, then perform modifications as needed 
+'''
+D4: change camera mode from color to grayscale
+input: void
+camera object from PiCamera, 
+return camera object set to grayscale
+'''
+def to_grayscale():
+    camera.color_effects = (128,128) # turn camera to black and white
+    return camera
+
+'''
+F6: rotate camera 180 degrees
+input: void
+currently the method also outputs a jpeg of rotated picture from the most recently picture taken
+return camera with 180 degree rotation
+'''
+def rotate():
+    camera.rotation = 270
+    list_of_files = glob.glob('/home/pi/WURocketry_Payload_2022-2023/main/Imaging/image_results/*.jpeg') # * means all if need specific format then *.csv
+    if not list_of_files:
+        return camera
+    
+    latest_file = max(list_of_files, key=os.path.getctime)
+    print(latest_file)
+    file_path = latest_file.replace('.jpeg','_rotated.jpeg')
+    print(file_path)
+
+    # using Pillow
+    
+    #read the image, not using OpenCV
+    img = Image.open(latest_file)
+    #rotate image
+    angle = 90
+    rotated = img.rotate(angle)
+    file_path = latest_file.replace('.jpg','_rotated.jpg')
+    rotated.save(file_path)
+
+    return rotated 
+
+# E5: change camera mode back from grayscale to color 
+def to_color_mode():
+    camera.color_effects = None
+    return camera
+
+'''
+H8: remove all filters 
+remove filter on taking pictures
+'''
+def remove_filter():
+    # remove special filter
+    camera.image_effect = 'none'
+    # reset color space
+    camera.color_effects = None
+    camera.rotation=90
+
+    list_of_files = glob.glob('/home/pi/WURocketry_Payload_2022-2023/main/Imaging/image_results/*.jpg') # * means all if need specific format then *.csv
+    if not list_of_files:
+        return camera
+   
+    list_of_files.sort(key=os.path.getctime)
+    print(list_of_files)
+    return camera
+
+
+'''
+G7: special effects filter to the image 
+input: void
+currently the method also outputs a jpeg of picture whose color space are switched from the most recently picture taken
+output: image with color space switched from RGB to BGR
+other options for special effect: 
+https://projects.raspberrypi.org/en/projects/getting-started-with-picamera/7
+'''
+def rgb2bgr():
+    camera.image_effect = 'colorswap'
+    list_of_files = glob.glob('/home/pi/WURocketry_Payload_2022-2023/main/Imaging/image_results/*.jpeg') # * means all if need specific format then *.csv
+    if not list_of_files:
+        return camera
+  
+    latest_file = max(list_of_files, key=os.path.getctime)
+    file_path = latest_file.replace('.jpeg','_special.jpeg')
+    img = Image.open(latest_file)
+    b, g, r = img.split()
+    switched = Image.merge("RGB", (r, g, b))
+    switched.save(file_path)
+    #file_path = str('./image_results/'+latest_file+'_special')
+    
+    return switched
+
+# move picture taken to image_results
+# C3: take picture
+def take_picture():
+    camera.rotation = 90
+    # Get the timezone object for Chicago
+    tz_cst = timezone('America/Chicago') 
+    # Get the current time in CST
+    datetime_cst = datetime.now(tz_cst)
+    # Format the time as a string 
+    local_time = datetime_cst.strftime("%H:%M:%S")
+    # get date 
+    local_date = datetime.now(timezone('US/Central'))  # use datetime here
+    today_date=local_date.date()  
+    #today_date = date.today()
+    #print("Today's date:", today_date, type(today_date))
+    annotation = str(today_date)+'_'+local_time
+    print("full annotation: ", annotation)
+    camera.annotate_text = annotation
+    sleep(5)
+    #store image
+    camera.capture('/home/pi/WURocketry_Payload_2022-2023/main/Imaging/image_results/'+annotation+'.jpeg')
+    print("picture just captured")
+    return camera
+
+
+if __name__ == '__main__':
+    take_picture()
+    rgb2bgr()
+    to_grayscale()
+    take_picture()
+    rotate()
+    to_color_mode()
+    take_picture()
+    remove_filter()
+    take_picture()
+    print("executing tasks in imaging.py")
+    
+
+# https://www.nasa.gov/sites/default/files/atoms/files/2023_slhandbook_508.pdf
+
+# add timestamp: format with date/time
+# save to disk, rename with timestamp...
+# C3 ~ H8
