e2298b9ad2e505048da7950da999d6e357b90175
Autumn Xu <autumn@wustl.edu>
2023-04-09 16:34:45 -0500

    tweaking avionics threshold

--- main/Avionics/sensing.py
+++ main/Avionics/sensing.py
@@ -330,6 +330,7 @@ functionality: detect whether the payload is moving up, or moving down or
 '''
 def altitude_status(altitude_accumulator, pressure_accumulator):
     global sea_level_altitude
+    global bmp_pointer
     rolling_window = 50
     descent_altitude = -6/100 # testing 4/9
     ascent_altitude =  6/100 # testing 4/9
@@ -344,8 +345,8 @@ def altitude_status(altitude_accumulator, pressure_accumulator):
     elif (altitude_differential > ascent_altitude and pressure_differential < ascent_pressure):
         print('BMP -- payload is moving down')
         return 'down'
-    elif (abs((average_window(altitude_accumulator, rolling_window)-sea_level_altitude)) > ascent_altitude 
-          and abs((average_window(pressure_accumulator, rolling_window)-bmp.sea_level_pressure)) > ascent_altitude):
+    elif (abs((average_window(altitude_accumulator, rolling_window, bmp_pointer)-sea_level_altitude)) > ascent_altitude 
+          and abs((average_window(pressure_accumulator, rolling_window, bmp_pointer)-bmp.sea_level_pressure)) > ascent_altitude):
         return 'moving'
     else:
         print('BMP -- indeterminant')  
