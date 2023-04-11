fc9f34b9e6f9c6af541bd0abec15ccf75328b478
Autumn Xu <autumn@wustl.edu>
2023-04-10 20:07:15 -0500

    changes

--- main/main.py
+++ main/main.py
@@ -355,7 +355,7 @@ def main():
             aprs_subprocess = APRS.begin_APRS_recieve(APRS_LOG_PATH) # Begin listening for APRS commands
 
 def test_main():
-   sys_flags.STAGE_INFO = Stage.LANDED
+   sys_flags.STAGE_INFO = Stage.PRELAUNCH
    #telemetryRoutine()
    while (True):
        avionicRoutine()
