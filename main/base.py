dae460b118001cc9a3ca0c9ec4a944738b0f0421
Autumn Xu <autumn@wustl.edu>
2023-04-10 19:14:14 -0500

    changes

--- main/base.py
+++ main/base.py
@@ -15,7 +15,7 @@ def test_base():
         rx_data = telemetryRoutine()
         encoding = 0
         receive_time = time.time_ns()
-        if rx_data != 0:
+        if rx_data != 0 and rx_data:
             encoding = str(Stage(rx_data[0]))+str(Movement(rx_data[1]))+str(Flight_Direction(rx_data[2]))+str(Verticality(rx_data[3]))+str(Separated(rx_data[4]))+str(Deployed(rx_data[5]))+str(Warn_Heat(rx_data[6]))+str(Warn_Camera(rx_data[7]))+str(Warn_Avionics(rx_data[8]))+str(Warn_Motive(rx_data[9]))
         f.write(f'{receive_time} <- {encoding}\n')
         print(f'{receive_time} <- {encoding}')  
